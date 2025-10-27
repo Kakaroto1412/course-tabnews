c192b95b139f87faf0f998eb31ce5fb812b2a6bb
DvDMR <231347712+Kakaroto1412@users.noreply.github.com>
2025-10-27 09:59:27 -0400

    new page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 function Home() {
   return (
     <h1>
-      Minha competição é comigo mesmo todos os dias. Meu maior adversário está
-      dentro de mim e a cada dia vou vence-lo...
+      Eu calço é 37 Meu pai me dá 36 Dói, mas no dia seguinte Aperto meu pé
+      outra vez Eu aperto meu pé outra vez Pai, eu já tô crescidinho Pague pra
+      ver, que eu aposto Vou escolher meu sapato E andar do jeito que eu gosto E
+      andar do jeito que eu gosto
     </h1>
   );
 }
