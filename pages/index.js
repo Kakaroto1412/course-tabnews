8195500057083a5137da6a95819462ef7f8aa334
DvDMR <231347712+Kakaroto1412@users.noreply.github.com>
2025-10-28 15:22:26 -0400

    add `.vercel`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 function Home() {
-  return (
-    <h1>
-      Eu calço é 37 Meu pai me dá 36 Dói, mas no dia seguinte Aperto meu pé
-      outra vez Eu aperto meu pé outra vez Pai, eu já tô crescidinho Pague pra
-      ver, que eu aposto Vou escolher meu sapato E andar do jeito que eu gosto E
-      andar do jeito que eu gosto
-    </h1>
-  );
+  return <SpeedInsights />;
 }
 
 export default Home;
