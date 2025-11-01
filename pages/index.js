309cc7e6327b543a1e50ebe06d780ebd224b8480
DvDMR <231347712+Kakaroto1412@users.noreply.github.com>
2025-11-01 04:56:22 -0400

    limpando

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-import function Home() {
-  return <h1>Hello World!</h1>;
+function Home() {
+  return (
+    <h1>
+      Eu calço é 37 Meu pai me dá 36 Dói, mas no dia seguinte Aperto meu pé
+      outra vez Eu aperto meu pé outra vez Pai, eu já tô crescidinho Pague pra
+      ver, que eu aposto Vou escolher meu sapato E andar do jeito que eu gosto E
+      andar do jeito que eu gosto
+    </h1>
+  );
 }
-
-export default Home;
