c61111cab4fb1e101f7ef36801ca37f5dc5c62fd
DvDMR <231347712+Kakaroto1412@users.noreply.github.com>
2025-11-04 19:15:20 -0400

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Eu calço é 37 Meu pai me dá 36 Dói, mas no dia seguinte Aperto meu pé
-      outra vez Eu aperto meu pé outra vez Pai, eu já tô crescidinho Pague pra
-      ver, que eu aposto Vou escolher meu sapato E andar do jeito que eu gosto E
-      andar do jeito que eu gosto
-    </h1>
-  );
+  return <h1>Seeds</h1>;
 }
 
 export default Home;
