22d9e1c4c46e6e544fc091a5c2ac7a13805fab9c
Kakaroto1412 <231347712+Kakaroto1412@users.noreply.github.com>
2025-11-09 10:41:22 -0400

    testes reais

---
+++ models/calculadora.js
@@ -0,0 +1,9 @@
+// test("espero que de certo", () => {
+//   expect(2).toBe(2);
+// });
+
+function somar(argumento1, argumento2) {
+  return argumento1; // argumento2;
+}
+
+exports.somar = somar;
--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,3 +1,10 @@
-test("espero que de certo", () => {
-  expect(2).toBe(2);
+// test("espero que de certo", () => {
+//   expect(2).toBe(2);
+// });
+
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 devera retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
 });
