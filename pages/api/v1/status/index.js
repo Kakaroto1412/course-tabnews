22f0f09ece08701a0d736a229a6356e6abc5ff2c
DvDMr <deividcursos@protonmail.com>
2025-11-16 21:04:39 -0400

    confoguraçao padrao

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,6 @@
 function status(request, respose) {
-  respose.status(200).json({ valor: "teste drive ^d~~" });
+  // respose.status(200).json({ valor: "teste drive ^d~~" });
+  return respose.redirect(307, "/new-route");
 }
 
 export default status;
