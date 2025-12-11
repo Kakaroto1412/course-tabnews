6bf2ac2a8c958a05676f0fc6fae1c2f9b6b07cf8
DvDMr <contato@deividmrdev.com.br>
2025-12-11 04:25:46 -0400

    finalizando /status

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  // const parseUpdateAt = new Date(responseBody.updated_at).toISOString();
+  // expect(responseBody.updated_at).toEqual(parseUpdateAt);
+
+  const date = new Date(responseBody.updated_at);
+  expect(date.toString()).not.toBe("Invalid Date");
+
+  expect(responseBody.dependencies.database.version).toEqual("16.11");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opend_connections).toEqual(1);
+
+  // expect(responseBody.dependencies.database.version).toBeDefined();
+  // expect(typeof responseBody.dependencies.database.version).toBe("string");
 });
