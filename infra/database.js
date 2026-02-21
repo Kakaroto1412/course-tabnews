642f667614afedb58c450a183d00d485a8829009
DvDMr <contato@deividmrdev.com.br>
2026-02-21 03:15:34 -0400

    fix-migrat

--- infra/database.js
+++ infra/database.js
@@ -17,7 +17,7 @@ async function getNewClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: false,
+    ssl: true,
   });
   await client.connect();
   return client;
