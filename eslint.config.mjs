b4b2a7cafc7858040e10f88efe93b8d809a18fb4
DvDMr <contato@deividmrdev.com.br>
2026-03-31 00:02:36 -0400

    `.github/workflows/linting.yaml` pretty good?

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,15 @@ export default defineConfig([
   prettier,
 
   {
+    extends: [
+      "eslint:recommended",
+      "plugin:jest/recommended",
+      "next/core-web-vitals",
+      "prettier",
+    ],
+  },
+
+  {
     files: ["**/*.test.js", "**/*.spec.js", "tests/**/*.js"],
     ...jest.configs["flat/recommended"],
   },
@@ -30,12 +39,3 @@ export default defineConfig([
 
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
-
-// {
-//   "extends": [
-//     "eslint:recommended",
-//     "plugin:jest/recommended",
-//     "next/core-web-vitals",
-//     "prettier"
-//   ]
-// }
